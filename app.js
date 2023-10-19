ff43d0d6d3a0001d6756d884bf627da309aa0b49
kimhyun <forhn37@gmail.com>
2023-10-19 17:34:19 +0900

    서버3 재시도 설정

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(500);
     return res.end('서버에 문제가 생겼습니다.')
   }
-
+// 로그인페이지 get형식 최초입장
   if (req.url === '/login') {
     fs.readFile('login/login.html', 'utf8', (err, data) => {
       if (err) {
@@ -23,6 +23,8 @@ const server = http.createServer((req, res) => {
       res.end(data);
     });
   };
+
+// 로그인 css 바로 설정하기 위해 get 호스팅
   if (req.url === '/login/css') {
     fs.readFile('login/login.css', 'utf8', (err, data) => {
       if (err) {
@@ -32,6 +34,7 @@ const server = http.createServer((req, res) => {
       res.end(data);
     });
   };
+  
 // login페이지에서 폼에서 만들어질 data를 담을 수단
   if(req.method === 'POST' && req.url === '/login') {
 // body 선언
@@ -44,12 +47,12 @@ const server = http.createServer((req, res) => {
       const parsedBody = querystring.parse(body);
       const {id, password, passwordcopy, email} = parsedBody;
 
-      console.log(parsedBody);
-      console.log(parsedBody.id);
-      console.log(parsedBody.password);
-      console.log(parsedBody.passwordcopy)
-      console.log(parsedBody.email);
-      console.log(signUpAsset)
+      // console.log(parsedBody);
+      // console.log(parsedBody.id);
+      // console.log(parsedBody.password);
+      // console.log(parsedBody.passwordcopy)
+      // console.log(parsedBody.email);
+      // console.log(signUpAsset)
 
       signUpAsset.id = parsedBody.id ;
       signUpAsset.password =parsedBody.password;
@@ -65,6 +68,7 @@ const server = http.createServer((req, res) => {
     })
     
   }
+  // board최초입장 get 호스팅
   if (req.url === '/board') {
     fs.readFile('board/board.html', 'utf8', (err, data) => {
       if (err) {
@@ -75,15 +79,7 @@ const server = http.createServer((req, res) => {
     });
   };
 
-  if (req.url === '/board/titletext') {
-    fs.readFile('board/titletext.js', 'utf8', (err, data) => {
-      if (err) {
-        serverErrorLog();wkqk
-      }
-      res.writeHead(200, { 'Content-Type': 'text/javascript' });
-      res.end(data);
-    });
-  };
+  // board에 post방식으로 내용 저장
   if(req.method === 'POST' && req.url === '/board') {
     // body 선언
         let body = '';
@@ -92,16 +88,6 @@ const server = http.createServer((req, res) => {
           body+= chunk.toString(); 
         })
         req.on('end', () => {
-          
-          fs.readFile('board/board.html', 'utf8', (err, data) => {
-            if (err) {
-              serverErrorLog();
-            }
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
-          });
-
-
           const parsedBody = querystring.parse(body);
           const {title, text} = parsedBody;
         
@@ -113,11 +99,6 @@ const server = http.createServer((req, res) => {
           titletext.text = parsedBody.text;
 
           console.log(titletext);
-
-
-
-          
-          
     // ? 테스트해야할 수단 'Content-Type': 'text/plain'//
           res.writeHead(200, {'Content-Type': 'text/plain' })
           res.end("hello");

2b95a348c1186964e0f5379671327cd78aaebf01
kimhyun <forhn37@gmail.com>
2023-10-19 10:55:42 +0900

    서버 구동테스트

--- app.js
+++ app.js
@@ -3,20 +3,23 @@ const http = require('http');
 const fs = require('fs');
 
 // 서버를 선언해 가독성위해 함
-const server = http.createServer((req,res) => {
+const server = http.createServer((req, res) => {
 
   function serverErrorLog() {
     res.writeHead(500);
     return res.end('서버에 문제가 생겼습니다.')
   }
 
-  if(req.url === '/Login') {
-    fs.readFile('./board/board.html', 'utf8', (err,data) => {
-      if(err) {
+  if (req.url === '/Login') {
+    fs.readFile('./board/board.html', 'utf8', (err, data) => {
+      if (err) {
         serverErrorLog();
       }
-      res.writeHead(200, {'Content-Type':'text/html'});
+      res.writeHead(200, { 'Content-Type': 'text/html' });
       res.end(data);
     });
-  } else if(req.url === '/Login' && req.method ==='POST')
+  };
 })
+server.listen(5050, () => {
+  console.log('서버 정상실행되었습니다')
+})  

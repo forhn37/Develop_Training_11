c4ec19008c37e6779f947849801b887f85cd6fd8
kimhyun <forhn37@gmail.com>
2023-10-20 00:59:23 +0900

    readfile없이 실시

--- app.js
+++ app.js
@@ -92,6 +92,74 @@ const server = http.createServer((req, res) => {
 
   // board에 post방식으로 내용 저장
   if (req.method === 'POST' && req.url === '/board') {
+      if (err) {
+        serverErrorLog();
+      }
+      // body 선언
+      let body = '';
+
+      req.on('data', (chunk) => {
+        body += chunk.toString();
+      })
+      req.on('end', () => {
+        const parsedBody = querystring.parse(body);
+        const { id, password, passwordcopy, email } = parsedBody;
+
+        console.log(parsedBody);
+        console.log(parsedBody.id);
+        console.log(parsedBody.password);
+        console.log(parsedBody.passwordcopy)
+        console.log(parsedBody.email);
+        console.log(signUpAsset);
+
+        signUpAsset.id = parsedBody.id;
+        signUpAsset.password = parsedBody.password;
+        signUpAsset.email = parsedBody.email;
+        
+        console.log(signUpAsset);
+
+        const datalist =`
+        <!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="http://localhost:8000/board/css"/>
+  <!-- <script src = "./titletext.js" defer></script> -->
+</head>
+
+<body>
+  <div id="root">
+    <div id ="header">
+      
+      <ul><span id = "idvalue">${signUpAsset.id}</span>님! 반갑습니다.<br>저에게 편지를 보내주세요!</ul>
+    </div>
+    <form action="/board" method="post">
+      <div>
+        <label for="title">Title</label>
+        <input type="text" name="title">
+      </div>
+      <div>
+        <label for="text">Text</label>
+        <input type="text" name="text" id="Text">
+      </div>
+      <input type="submit" name="send" id ="send">
+    </form>
+  </div>
+</body>
+</html>`
+
+              
+        res.writeHead(301, { 'Content-Type': 'text/html' })
+        res.end(datalist)
+      
+    })
+
+  }
+
+  if (req.method === 'POST' && req.url === '/board') {
     fs.readFile('board/board2.html', 'utf-8', (err, data) => {
       if (err) {
         serverErrorLog();

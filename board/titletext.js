3222611981731701622d5f5f5e242e2af4cb23e1
kimhyun <forhn37@gmail.com>
2023-10-19 15:48:53 +0900

    id값 불러오는 데 문제발생

--- board/titletext.js
+++ board/titletext.js
@@ -1,7 +1,16 @@
+const signUpAsset = require("../login/signUpAsset");
+
+let idvalue = document.getElementById('idvalue')
+
 const titletext  = {
   title: "",
   text: "",
   }
   
   
+  
+  idvalue.textContent = signUpAsset.id;
+  
+
+
   module.exports = titletext;

9e27924b91019c5a1d0a2eb943672f4e10fa4f1d
kimhyun <forhn37@gmail.com>
2023-10-20 14:15:22 +0900

    indexof로 이메일 형식 검사 문제 발생

--- login/indexof.js
+++ login/indexof.js
@@ -1,3 +1,12 @@
-let inputs = document.querySelector('input');
+// let input3 = input[3].textContent;
+// let input3index = input3.indexOf("@");
 
-input[3].textContent.IndexOf(".", )
+
+let indexof = function(input3) {
+if( input3.indexOf(".", input3.indexOf("@")) >0) {
+  return console.log(true);
+} else {
+  return console.log(false);
+}
+}
+indexof("asdfasdfasdf.");

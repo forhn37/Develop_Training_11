55639f4c93a5f877e74284f2791953ff61d9dd22
kimhyun <forhn37@gmail.com>
2023-10-20 14:20:36 +0900

    indexof의 검사

--- login/indexof.js
+++ login/indexof.js
@@ -2,11 +2,13 @@
 // let input3index = input3.indexOf("@");
 
 
-let indexof = function(input3) {
-if( input3.indexOf(".", input3.indexOf("@")) >0) {
-  return console.log(true);
-} else {
-  return console.log(false);
-}
+let indexof = function (input3) {
+  if (input3.indexOf("@") > 0 && input3.indexof(".") > 0) {
+    if (input3.indexof("@") < input3.indexof(".")) {
+      console.log("true");
+    } else {
+      console.log("false")
+    }
+  }
 }
 indexof("asdfasdfasdf.");

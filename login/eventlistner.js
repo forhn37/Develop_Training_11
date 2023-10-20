7a08f08e77a029e08f76317c5624088feae1e505
kimhyun <forhn37@gmail.com>
2023-10-20 15:28:09 +0900

    3째 패스워드 모듈 실험중

--- login/eventlistner.js
+++ login/eventlistner.js
@@ -1,11 +1,8 @@
+const indexlist = require('./indexof')
+const alpabetlist = require('./alpabet')
 
-// const submit = document.getElementById('submit');
-// const input = document.querySelectorAll("input")
-const indexlist = require('./indexof.js')
-const alpabetlist = require('./alpabet.js')
-
-// console.dir(submit);
-// console.dir(input);
+console.dir(submit);
+console.dir(input);
 
 // input[4].addEventListener('click', function() {
 //   if()
@@ -14,3 +11,4 @@ const alpabetlist = require('./alpabet.js')
 
 indexlist("20340230402304");
 alpabetlist("34534534534")
+

e63342ea8ef5f2c077b6f40059ee19f664751de5
kimhyun <forhn37@gmail.com>
2023-10-21 14:53:11 +0900

    정규식 외 방법 없다고 판단됨

--- login/signUpAsset.js
+++ login/signUpAsset.js
@@ -1,8 +1,8 @@
 const signUpAsset  = {
-        id: "qjqmfgus",
-        password: "1234",
-        passwordcopy: "1234",
-        email: "qjqmfgus@naver.com",
+        id: "ㅁㄴㅇㄻㄴㅇㄹ",
+        password: "ㅁㄴㅇㄹ",
+        passwordcopy: "ㅁㄴㅇㄹ",
+        email: "ㅁㄴㅇㄹ",
         inputBoxColor: "#D9D9D9",
         textColor: "#B6B6B6",
         pointColor: "#FF6666"
